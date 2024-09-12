bb0321fbea90ca5fb25d01b878c4217c58727be6
DAVID LIN <908060260@QQ.COM>
2024-09-12 21:38:46 +0800

    N

--- DVLib/LabDataHelper/MathObjectSystem/TextParser.cs
+++ DVLib/LabDataHelper/MathObjectSystem/TextParser.cs
@@ -5,27 +5,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Images;
+using DVOSLib;
+using DVLib.LabDataHelper.MathObjectSystem;
 
 namespace DVLib.LabDataHelper
 {
     public delegate  T  Factory<T,InfoT,ManagerT> (string text, ScanInfo<T,InfoT,ManagerT> scanInfo, List<ScanInfo< T,InfoT, ManagerT>> list,ManagerT Paser)where InfoT:ObjectInfo<T,InfoT,ManagerT>,new() where ManagerT:ObjectManager<T,InfoT,ManagerT>;
     public delegate bool OperatorInfoCondition<T, InfoT, M>(InfoT info) where InfoT : ObjectInfo<T, InfoT, M>, new() where M : ObjectManager<T, InfoT, M>;
+	public enum OperatorType
+	{
+		LeftRight, Left, Right, Func, Source, LeftRightOrRight,RUNCODE,RETURN
+	}
 
-    public class ScanInfo<T,InfoT,M>where InfoT : ObjectInfo<T,InfoT,M>,new()where M:ObjectManager<T,InfoT,M>
+
+
+	public class ScanInfo<T,I,M>where I : ObjectInfo<T,I,M>,new()where M:ObjectManager<T,I,M>
     {
-        internal ScanInfo(int p, int l, InfoT o)
+        internal ScanInfo(int p, int l, I o)
         {
             position = p;
             level = l;
             operatorInfo = o;
-
+            tag=o.tag;
         }
         internal int position;
         internal int fixedPosition { get { if (operatorInfo.reverse) return -position; return position; } }
         internal int level;
-        internal InfoT operatorInfo;
+        public string tag { get; private set; }
+        internal I operatorInfo;
     }
 
+    public class LevelGetter
+    {
+        Dictionary<char,int> map=new Dictionary<char, int>();
+
+        public LevelGetter(params (char Char,int level)[] values) {
+        foreach(var v in values)
+            {
+                register(v.Char, v.level);
+            }
+        }
+        public void register(char c,int l)
+        {
+            map.Add(c, l);
+        }
+
+        public int getLevel(char c)
+        {
+          if(map.TryGetValue(c, out int l)) return l;
+            return 0;
+        }
+    }
+
+    public class ScanResult
+    {
+         HashSet<OperatorType> types=new HashSet<OperatorType>();
+        HashSet<string> names=new HashSet<string>();
+
+        internal void add(OperatorType type)
+        {
+            types.Add(type);
+		}
+		internal void add(string type)
+		{
+			names.Add(type);
+		}
+		public bool containsType(OperatorType type)
+        {
+            return types.Contains(type);
+        }
+    
+        public bool containsName(string name)
+        {
+            return names.Contains(name);
+        }
+    }
+    public class CodeBlockInfo<T, I, M> where I : ObjectInfo<T, I, M>, new() where M : ObjectManager<T, I, M>
+	{
+       public (string name, List<ScanInfo<T, I, M>> infos)? code { get; private set; }
+		public (string name, List<ScanInfo<T, I, M>> infos)[] vars { get; private set; }
+		public CodeBlockInfo((string name, List<ScanInfo<T, I, M>> infos)? code, params (string name, List<ScanInfo<T, I, M>> infos)[] vars)
+        {
+            this.vars = vars;
+            this.code = code;
+        }
+    }
     public class ObjectInfo<T,S,M>where S:ObjectInfo<T,S,M>,new() where M:ObjectManager<T,S,M>
     {
     
@@ -33,10 +97,11 @@ namespace DVLib.LabDataHelper
         public string tag { get; internal set; }
         public string mark { get; internal set; }
         public int priority { get; internal set; }
-        public char dot { get; internal set; }
+        public char dot { get; internal set; } = ',';
         public bool reverse { get; internal set; }
         public OperatorType type { get; internal set; }
         public Factory<T,S,M> factory { get; internal set; }
+
 
         public ObjectInfo()
         {
@@ -63,16 +128,246 @@ namespace DVLib.LabDataHelper
             return (S)this;
         }
 
+        
+		public static CodeBlockInfo<T, I, M> solveCodeBlock<T, I, M>(string text, ScanInfo<T, I, M> ois, List<ScanInfo<T, I, M>> infos, MathObjectManager manager) where I : ObjectInfo<T, I, M>, new() where M : ObjectManager<T, I, M>
+		{
+			int r = ois.operatorInfo.mark.Length;
+            (string, List<ScanInfo<T, I, M>>)? code= null;
+			if (text.Length <= r)
+			{
+				return new CodeBlockInfo<T, I, M>(null);
+			}
+            string name=text.Substring(0, r);
+			text = text.Substring(r);
 
-    }
-    public class ObjectManager<T,InfoT,M>where InfoT:ObjectInfo<T,InfoT,M> ,new() where M:ObjectManager<T,InfoT,M>
+            if (text[0]=='(')
+            {
+                int p = text.AsSpan().findEnd('(', ')');
+                if(p<text.Length)
+                {
+                    if(p<text.Length - 1)
+                    {
+                      string part2= text.Substring(p+1);
+                        if (part2.StartsWith('{'))
+                        {
+                                List<ScanInfo<T, I, M>> l = new List<ScanInfo<T, I, M>>();
+                                ScanInfo<T, I, M> temp;
+
+
+								for (int i = 0;i<infos.Count;i++)
+                                {
+                                    temp= infos[i];
+                                    if (infos[i].position-r>p)
+                                    {
+                                        l.Add(temp);
+                                        temp.position -= name.Length + p + 1 ;
+                                        infos.RemoveAt(i);
+                                        i--;
+                                    }
+                                }
+                                code = (part2, l);
+                            }
+                        
+					}
+                    text = text.Substring(0, p+1);
+					int tt = Helper.clean(ref text);
+					List<int> pos = Helper.findDot(text, ois.operatorInfo.dot);
+					int funcSize = pos.Count + 1;
+					List<ScanInfo<T, I, M>>[] left = new List<ScanInfo<T, I, M>>[funcSize];
+					for (int i = 0; i < funcSize; i++)
+					{
+						left[i] = new List<ScanInfo<T, I, M>>();
+					}
+
+
+					foreach (var v in infos)
+					{
+						v.level -= tt;
+						v.position -= r;
+						bool notFound = true;
+						for (int i = pos.Count - 1; i >= 0; i--)
+						{
+							if (v.position > pos[i])
+							{
+								notFound = false;
+								left[i + 1].Add(v);
+								v.position -= pos[i] + 1 + tt;
+								break;
+							}
+						}
+						if (notFound)
+						{
+							left[0].Add(v);
+							v.position -= tt;
+						}
+					}
+
+					(string, List<ScanInfo<T, I, M>>)[] mathObjects = new (string, List<ScanInfo<T, I, M>>)[funcSize];
+					int id = 0;
+					for (int i = 0; i < funcSize; i++)
+					{
+						if (i < pos.Count)
+						{
+							mathObjects[i] = (text.Substring(id, pos[i] - id), left[i]);
+							id = pos[i] + 1;
+						}
+						else
+						{
+
+							mathObjects[i] = (text.Substring(id), left[i]);
+						}
+
+					}
+                    return new CodeBlockInfo<T, I, M>(code, mathObjects);
+                }
+
+            }
+            return new CodeBlockInfo<T, I, M>(null);
+			
+		}
+		public  static (string name, List<ScanInfo<T, I, M>> infos)[] solveFunc<T, I, M>(string text, ScanInfo<T, I, M> ois, List<ScanInfo<T, I, M>> infos, MathObjectManager manager) where I : ObjectInfo<T, I, M>, new() where M : ObjectManager<T, I, M>
+		{
+			int r = ois.operatorInfo.mark.Length;
+			if (text.Length <= r)
+			{
+				return new (string name, List<ScanInfo<T, I, M>> infos)[0];
+			}
+			text = text.Substring(r);
+
+            if(text.StartsWith('('))
+            {
+ int end = text.AsSpan().findEnd('(', ')');
+            if(end<text.Length)
+                {
+    text=text.Substring(0, end+1);
+
+	int tt = Helper.clean(ref text);
+			List<int> pos = Helper.findDot(text, ois.operatorInfo.dot);
+			int funcSize = pos.Count + 1;
+			List<ScanInfo<T, I, M>>[] left = new List<ScanInfo<T, I, M>>[funcSize];
+			for (int i = 0; i < funcSize; i++)
+			{
+				left[i] = new List<ScanInfo<T, I, M>>();
+			}
+
+
+			foreach (var v in infos)
+			{
+				v.level -= tt;
+				v.position -= r;
+				bool notFound = true;
+				for (int i = pos.Count - 1; i >= 0; i--)
+				{
+					if (v.position > pos[i])
+					{
+						notFound = false;
+						left[i + 1].Add(v);
+						v.position -= pos[i] + 1 + tt;
+						break;
+					}
+				}
+				if (notFound)
+				{
+					left[0].Add(v);
+					v.position -= tt;
+				}
+			}
+
+			(string, List<ScanInfo<T, I, M>>)[] mathObjects = new (string, List<ScanInfo<T, I, M>>)[funcSize];
+			int id = 0;
+			for (int i = 0; i < funcSize; i++)
+			{
+				if (i < pos.Count)
+				{
+					mathObjects[i] = (text.Substring(id, pos[i] - id), left[i]);
+					id = pos[i] + 1;
+				}
+				else
+				{
+
+					mathObjects[i] = (text.Substring(id), left[i]);
+				}
+
+			}
+			return mathObjects;
+                }
+            }
+
+            return new (string name, List<ScanInfo<T, I, M>> infos)[0];
+           
+
+		
+		}
+
+		public static (string name, List<ScanInfo<T,I,M>> infos)[] solveLR<T,I,M>(string text, ScanInfo<T,I,M> ois, List<ScanInfo<T,I,M>> infos, MathObjectManager manager)where I:ObjectInfo<T,I,M>,new() where M:ObjectManager<T,I,M>
+		{
+			List<ScanInfo<T, I, M>> left = new List<ScanInfo<T, I, M>>();
+			List<ScanInfo<T, I, M>> right = new List<ScanInfo<T, I, M>>();
+			int l = ois.position;
+			int r = ois.position + ois.operatorInfo.mark.Length;
+			foreach (var v in infos)
+			{
+				if (v.position < l)
+				{
+					left.Add(v);
+				}
+				else if (v.position >= r)
+				{
+					v.position -= r;
+					right.Add(v);
+				}
+			}
+			return new (string, List<ScanInfo<T, I, M>>)[]{(text.Substring(0, l), left),
+				(text.Substring(r), right)};
+		}
+		public static (string name, List<ScanInfo<T, I, M>> infos) solveL<T, I, M>(string text, ScanInfo<T, I, M> ois, List<ScanInfo<T, I, M>> infos, MathObjectManager manager) where I : ObjectInfo<T, I, M>, new() where M : ObjectManager<T, I, M>
+		{
+			List<ScanInfo<T, I, M>> left = new List<ScanInfo<T, I, M>>();
+			int l = ois.position;
+			foreach (var v in infos)
+			{
+				if (v.position < l)
+				{
+					left.Add(v);
+				}
+			}
+			return (text.Substring(0, l), left);
+		}
+		public static (string name, List<ScanInfo<T, I, M>> infos) solveR<T, I, M>(string text, ScanInfo<T, I, M> ois, List<ScanInfo<T, I, M>> infos, MathObjectManager manager) where I : ObjectInfo<T, I, M>, new() where M : ObjectManager<T, I, M>
+		{
+			List<ScanInfo<T, I, M>> right = new List<ScanInfo<T, I, M>>();
+
+			int r = ois.operatorInfo.mark.Length;
+			foreach (var v in infos)
+			{
+				if (v.position >= r)
+				{
+					v.position -= r;
+					right.Add(v);
+				}
+			}
+			return (text.Substring(r), right) ;
+		}
+
+
+	}
+	public class ObjectManager<T,InfoT,M>where InfoT:ObjectInfo<T,InfoT,M> ,new() where M:ObjectManager<T,InfoT,M>
     {
-        static int max = 6;
-        static int maxPriority = 0;
+        
+       public int maxPriority { get; private set; } = 0;
         static Dictionary<char, HeadCharSet<T,InfoT,M>> stringLic = new Dictionary<char, HeadCharSet<T, InfoT,M>>();
         bool canOver = true;
+        public LevelGetter levelGetter { get; private set; } = new LevelGetter();
+
+        public ObjectManager()
+        {
+            registerLG(levelGetter);
+            registerDefault();
+        }
    public InfoT register(InfoT info)
         {
+
+
             if (info.priority + 1 > maxPriority)
             {
                 maxPriority = info.priority + 1;
@@ -92,8 +387,19 @@ namespace DVLib.LabDataHelper
             }
             return null;
         }
+        public virtual void registerLG(LevelGetter getter)
+        {
 
-        public InfoT match(string name)
+        }
+        public virtual void registerDefault()
+        {
+
+        }
+        public virtual string formalizeCode(string s)
+        {
+            return s;
+        }
+		public InfoT match(string name)
         {
             if (name.Length > 0)
             {
@@ -176,7 +482,7 @@ namespace DVLib.LabDataHelper
   
 
      
-        public T GetObject(string text, List<ScanInfo<T,InfoT,M>> infos)
+        public virtual T  GetObject(string text, List<ScanInfo<T,InfoT,M>> infos)
         {
             int t = Helper.clean(ref text);
             List<ScanInfo<T,InfoT,M >>[] plist = new List<ScanInfo<T,InfoT, M>>[maxPriority];
@@ -243,55 +549,49 @@ namespace DVLib.LabDataHelper
         {
             return default(T);
         }
-        public void ScanForOperators(string text, List<ScanInfo<T,InfoT,M>> list)
+        public ScanResult ScanForOperators(ref string text, List<ScanInfo<T,InfoT,M>> list)
         {
-
+            ScanResult r = new ScanResult();
+            bool hasCode=false;
+            text=formalizeCode(text);
             ReadOnlySpan<char> text_ = text.AsSpan();
-            int leftParentheses = 0;
-            int rightParentheses = 0;
-            int level;
+        
+            int level=0;
             ScanInfo<T,InfoT,M> osi;
             char c;
             HeadCharSet<T,InfoT,M> hs;
             InfoT info;
             for (int i = 0; i < text_.Length; i++)
             {
-                if (text_[i] == '(')
-                {
-                    leftParentheses++;
-                }
-                if (text_[i] == ')')
-                {
-                    rightParentheses++;
-                }
                 c = text_[i];
                 if (stringLic.TryGetValue(c, out hs))
                 {
                     info = hs.Match(text_.Slice(i));
                     if (info != null)
                     {
-                        level = leftParentheses - rightParentheses;
+                        if(!hasCode&&info.type==OperatorType.RUNCODE)
+                        {
+                            hasCode = true;
+                        }
                         osi = new ScanInfo<T,InfoT,M>(i, level, info);
                         list.Add(osi);
                         i += info.mark.Length - 1;
+                        r.add(info.type);
+                        r.add(info.mark);
                     }
 
                 }
+                level += levelGetter.getLevel(c);
             }
+            return r;
         }
     }
-    public class HeadCharSet<T,InfoT,M>where InfoT:ObjectInfo<T,InfoT,M>,new() where M:ObjectManager<T,InfoT,M>
+    public class HeadCharSet<T,InfoT,M>:CharDictionary<InfoT> where InfoT:ObjectInfo<T,InfoT,M>,new() where M:ObjectManager<T,InfoT,M>
     {
-        internal char head;
-        internal int maxLength;
-        internal int capacity = 8;
-        Dictionary<string,InfoT>[] context;
+      
 
-        public HeadCharSet(char head)
+        public HeadCharSet(char head):base(head)
         {
-            this.head = head;
-            this.maxLength = 0;
-            this.context = new Dictionary<string, InfoT>[capacity];
         }
 
         public void clear()
