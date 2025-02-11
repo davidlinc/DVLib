dd8206efdf205cbde01cfd778fffed16530b1206
DAVID LIN <908060260@QQ.COM>
2025-02-12 00:01:49 +0800

    A

--- DVLib/LabDataHelper/DVScript/ScriptHelper.cs
+++ DVLib/LabDataHelper/DVScript/ScriptHelper.cs
@@ -197,19 +197,7 @@ else
 
 		}
 
-		internal static ScriptF Void()
-		{
-			return
-
-			(string text, ScanInfo ois, List<ScanInfo> infos, ScriptManager manager, ScanResult r) =>
-			{
-
-				return new RootElementScriptObject(typeof(void), Expression.Label(Expression.Label(typeof(void))));
-
-				throw new ParamsCountMismatchException();
-			};
-		}
-
+	
 		internal static ScriptF Void()
 		{
 			return
